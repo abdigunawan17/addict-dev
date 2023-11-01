50b4f90b6fbd2b15e50f1d5296cf2bb57040c823
Putu Abdi Gunawan <user@Abdi-Gunawan-MacBook-Pro.local>
2023-11-02 07:43:07 +0800

    add cron job

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,12 @@ export async function middleware(request: NextRequest) {
         pathname.startsWith("/api/users"), 
         pathname.startsWith("/api/posts"),
         pathname.startsWith("/api/follows"),
+        pathname.startsWith("/api/admin"),
+        pathname.startsWith("/api/search"),
+    ];
+
+    const authenticatedCronRoutes = [
+        pathname.startsWith("/api/cron")
     ];
 
     if (authenticatedAPIRoutes.includes(true)) {
@@ -32,6 +38,14 @@ export async function middleware(request: NextRequest) {
                 status: 401
             });
 
+        }
+    }
+
+    if(authenticatedCronRoutes.includes(true)) {
+        const key = request.nextUrl.searchParams.get("cron_api_key");
+        const isAuthenticated = key === process.env.CRON_API_KEY;
+        if(!isAuthenticated) {
+            return NextResponse.json({ error: "Sorry unauthenticated"}, { status: 401 });
         }
     }
 }
